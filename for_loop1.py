deee1990168c9805dfe280bf5a04cf99bbbd7e4c
Edwin Gabula <138397826+HyperReal256@users.noreply.github.com>
2025-09-09 03:35:45 +0300

    Delete for_loop1.py

--- for_loop1.py
+++
@@ -1,9 +0,0 @@
-#displaying numbers and their square roots
-
-print("Number", '\t', "Square")
-print("------------------")
-
-
-for num in range (1, 11):           #calling the for loop
-    square = num ** 2
-    print(num, '\t', square)
